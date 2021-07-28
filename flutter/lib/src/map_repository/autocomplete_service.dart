f740aa5b41d19fa2d660c0667baed58a98117074
Aaqib Ismail <aaqibism@usc.edu>
2021-07-28 11:01:55 -0700

    Native javascript handling for client side Maps API

---
+++ flutter/lib/src/map_repository/autocomplete_service.dart
@@ -0,0 +1,39 @@
+// @JS()
+// library maps;
+
+// import 'package:js/js.dart';
+
+// @JS('JSON.stringify')
+// external String stringify(Object obj);
+
+// @JS('google.maps.LatLng')
+// class JSLatLng {
+//   external JSLatLng(num lat, num lng);
+// }
+
+// @JS('google.maps.places.AutocompleteService')
+// class AutocompleteService {
+//   external AutocompleteService();
+//   external Object getPlacePredictions(
+//     AutocompleteRequest obj,
+//   );
+// }
+
+// @JS('google.maps.places.AutocompleteRequest')
+// @anonymous
+// class AutocompleteRequest {
+//   external String? get input;
+//   external set input(String? i);
+
+//   external JSLatLng? get location;
+//   external set location(JSLatLng? l);
+
+//   external num? get radius;
+//   external set radius(num? r);
+
+//   external factory AutocompleteRequest({
+//     String input,
+//     JSLatLng? location,
+//     num? radius,
+//   });
+// }
